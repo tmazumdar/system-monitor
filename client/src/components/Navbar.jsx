a0132c6df871c480a6b9b1a18b1135a09e8c542d
Tahsin Mazumdar <tahsin.mazumdar@gmail.com>
2025-12-18 18:44:58 -0500

    move image asset to public directory

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     <div>
       <nav className="flex justify-between items-center mb-6">
         <NavLink to="/">
-          <img className="h-10 inline" src="src/assets/raspberry-pi.png"></img>
+          <img className="h-10 inline" src="/raspberry-pi.png"></img>
         </NavLink>
       </nav>
     </div>
