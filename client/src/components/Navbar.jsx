e18c3bfb35018f83c3767ae3809fb800899ba786
Tahsin Mazumdar <tahsin.mazumdar@gmail.com>
2025-12-18 19:20:51 -0500

    fix logo location

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import { NavLink } from "react-router-dom";
+import rpiLogo from "../assets/raspberry-pi.png";
 
 export default function Navbar() {
   return (
     <div>
       <nav className="flex justify-between items-center mb-6">
         <NavLink to="/">
-          <img className="h-10 inline" src="/raspberry-pi.png"></img>
+          <img className="h-10 inline" src={rpiLogo}></img>
         </NavLink>
       </nav>
     </div>
